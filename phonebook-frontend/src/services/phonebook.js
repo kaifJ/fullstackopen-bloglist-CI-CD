6368878599bba3c28909928efb199c169b4f4ed1
kaifJ <kaif.jamadar.in@gmail.com>
2023-03-23 21:09:15 +0200

    fix eslint issue

--- phonebook-frontend/src/services/phonebook.js
+++ phonebook-frontend/src/services/phonebook.js
@@ -22,4 +22,5 @@ const deletePerson = (id) => {
     return request.then(response => response.data)
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { getAll, create, deletePerson, updatePhonebook }
